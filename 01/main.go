4936b86452b4ec7274862ebceebab00347fdccbf
holdenparker <holdenparker55@gmail.com>
2025-12-02 21:00:34 -0500

    feat: 01 - Counting the number of times we passed 0.

--- 01/main.go
+++ 01/main.go
@@ -8,20 +8,33 @@ import (
 )
 
 func main() {
+	// filename := "test.txt"
+	filename := "data.txt"
+
 	part01 := LockDial{
 		Pos:    50,
 		Zeroes: 0,
 	}
-	// err := process_file("test.txt", part01.PartOne)
-	err := process_file("data.txt", part01.PartOne)
+	err := process_file(filename, part01.PartOne)
 	if err != nil {
 		fmt.Printf("Error with part 1!\n%v\n", err)
 	} else {
 		fmt.Printf("Part 1: %v\n", part01.Zeroes)
 	}
+
+	part02 := LockDial{
+		Pos:    50,
+		Zeroes: 0,
+	}
+	err = process_file(filename, part02.PartTwo)
+	if err != nil {
+		fmt.Printf("Error with part 1!\n%v\n", err)
+	} else {
+		fmt.Printf("Part 2: %v\n", part02.Zeroes)
+	}
 }
 
-type ProcessLine func(string) error
+type ProcessLine func(string, int) error
 
 func process_file(filename string, procl ProcessLine) error {
 	file, err := os.Open(filename)
@@ -34,7 +47,11 @@ func process_file(filename string, procl ProcessLine) error {
 
 	for scanner.Scan() {
 		line := scanner.Text()
-		err = procl(line)
+		dir, mag, err := read_line(line)
+		if err != nil {
+			return err
+		}
+		err = procl(dir, mag)
 
 		if err != nil {
 			return err
@@ -53,12 +70,8 @@ type LockDial struct {
 	Zeroes int
 }
 
-func (ld *LockDial) PartOne(line string) error {
+func (ld *LockDial) PartOne(dir string, mag int) error {
 	zeroes := ld.Zeroes
-	dir, mag, err := read_line(line)
-	if err != nil {
-		return err
-	}
 
 	ld.AdjustPos(dir, mag)
 
@@ -70,22 +83,33 @@ func (ld *LockDial) PartOne(line string) error {
 	return nil
 }
 
+func (ld *LockDial) PartTwo(dir string, mag int) error {
+	ld.AdjustPos(dir, mag)
+	return nil
+}
+
 func (ld *LockDial) AdjustPos(dir string, mov int) {
 	switch dir {
 	case "R":
 		ld.Pos += mov
+		for ld.Pos > 99 {
+			ld.Zeroes += 1
+			ld.Pos -= 100
+		}
 	case "L":
+		if ld.Pos == 0 {
+			ld.Zeroes -= 1
+		}
 		ld.Pos -= mov
+		for ld.Pos < 0 {
+			ld.Zeroes += 1
+			ld.Pos += 100
+		}
+		if ld.Pos == 0 {
+			ld.Zeroes += 1
+		}
 	}
 
-	for ld.Pos < 0 {
-		ld.Zeroes += 1
-		ld.Pos += 100
-	}
-	for ld.Pos > 99 {
-		ld.Zeroes += 1
-		ld.Pos -= 100
-	}
 }
 
 func read_line(line string) (string, int, error) {
