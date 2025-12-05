d976d60c740ff4c37e7d735859e924f86c17700d
holdenparker <holdenparker55@gmail.com>
2025-12-04 22:49:15 -0500

    chore: util - Removing Init and Run ProcessFile methods.

--- util/process.file.go
+++ util/process.file.go
@@ -10,34 +10,6 @@ type ProcessFile struct {
 	Scanner  *bufio.Scanner
 	Process  func(string) error
 	Split    bufio.SplitFunc
-	file     *os.File
-}
-
-func (pf *ProcessFile) Init() error {
-	file, err := os.Open(pf.Filename)
-	if err != nil {
-		return err
-	}
-
-	pf.file = file
-	pf.Scanner = bufio.NewScanner(pf.file)
-	return nil
-}
-
-func (pf *ProcessFile) Run() error {
-	defer pf.file.Close()
-
-	for pf.Scanner.Scan() {
-		segment := pf.Scanner.Text()
-		err := pf.Process(segment)
-		if err != nil {
-			return err
-		}
-	}
-	if err := pf.Scanner.Err(); err != nil {
-		return err
-	}
-	return nil
 }
 
 func (pf *ProcessFile) Go() error {
