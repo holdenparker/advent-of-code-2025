0366e397e962698d1589ccf7b378f53c0386b1a4
holdenparker <holdenparker55@gmail.com>
2025-12-09 09:45:01 -0500

    feat: 07 - Counting the number of tachyon manifold timelines.

--- 07/main.go
+++ 07/main.go
@@ -19,39 +19,48 @@ func main() {
 		fmt.Printf("Error with part 1!\n%v\n", err)
 	} else {
 		fmt.Printf("Number of splits: %v\n", tmb.Splits)
+		fmt.Printf("Number of timelines: %v\n", tmb.Timelines())
 	}
 }
 
 type TachyonManifoldBeams struct {
-	beams  []bool
+	beams  []int
 	Splits int
 }
 
 func (tmb *TachyonManifoldBeams) NextLine(line string) error {
 	for len(tmb.beams) < len(line) {
-		tmb.beams = append(tmb.beams, false)
+		tmb.beams = append(tmb.beams, 0)
 	}
 	for i, c := range line {
 		switch c {
 		case '.':
 		case '^':
-			if tmb.beams[i] {
+			if tmb.beams[i] > 0 {
 				tmb.split(i)
 			}
 		case 'S':
-			tmb.beams[i] = true
+			tmb.beams[i] = 1
 		}
 	}
 	return nil
 }
 
 func (tmb *TachyonManifoldBeams) split(i int) {
-	tmb.beams[i] = false
 	if i-1 >= 0 {
-		tmb.beams[i-1] = true
+		tmb.beams[i-1] += tmb.beams[i]
 	}
 	if i+1 < len(tmb.beams) {
-		tmb.beams[i+1] = true
+		tmb.beams[i+1] += tmb.beams[i]
 	}
+	tmb.beams[i] = 0
 	tmb.Splits++
+}
+
+func (tmb *TachyonManifoldBeams) Timelines() int {
+	result := 0
+	for _, t := range tmb.beams {
+		result += t
+	}
+	return result
 }
